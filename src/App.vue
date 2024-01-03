f27b8d49f85fb0f8ce8291717abfc6291145e99e
Mateus Cavedini Pereira <mateuscavedini@gmail.com>
2024-01-03 03:41:10 -0300

    Adicionados componentes e views

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,20 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
+    <Menu/>
     <router-view/>
   </div>
 </template>
+
+<script>
+import Menu from '@/components/Menu.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Menu
+  }
+}
+</script>
 
 <style>
 #app {
@@ -15,18 +23,5 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
---
+++ src/views/WarehouseList.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Galpões Cadastrados</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WarehouseList'
+}
+</script>
+
+<style></style>
