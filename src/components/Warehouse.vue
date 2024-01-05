1e032814af885a3c241f86d4eb5e1b2004f645bd
Mateus Cavedini Pereira <mateuscavedini@gmail.com>
2024-01-05 03:15:11 -0300

    Exibe galpões cadastrados

---
+++ src/components/Warehouse.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="card">
+    <h3>{{ name }} - {{ code }}</h3>
+    <address>
+      <strong>Endereço: {{ address }}</strong>
+      <p>Cidade: {{ city }}</p>
+      <p>Área total: {{ area }}m²</p>
+    </address>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Warehouse',
+  props: {
+    id: Number,
+    name: String,
+    code: String,
+    address: String,
+    city: String,
+    area: Number
+  }
+}
+</script>
+
+<style>
+.card {
+  width: 30%;
+  border-style: solid;
+  border-radius: 25px;
+  margin-bottom: 10px;
+  margin-left: auto;
+  margin-right: auto;
+}
+</style>
--- src/views/WarehouseList.vue
+++ src/views/WarehouseList.vue
@@ -1,12 +1,45 @@
 <template>
   <div>
     <h1>Galpões Cadastrados</h1>
+
+    <div v-for="w in warehouses" :key="w.id">
+      <Warehouse
+        :id = "w.id"
+        :name = "w.name"
+        :code = "w.code"
+        :address = "w.address"
+        :city = "w.city"
+        :area = "w.area"
+      />
+    </div>
   </div>
 </template>
 
 <script>
+import Warehouse from '@/components/Warehouse.vue'
+
 export default {
-  name: 'WarehouseList'
+  name: 'WarehouseList',
+  components: {
+    Warehouse
+  },
+  data() {
+    return {
+      warehouses: []
+    }
+  },
+  async mounted() {
+    this.getWarehouses()
+  },
+  methods: {
+    async getWarehouses() {
+      const response = await this.$http.get('http://localhost:3000/api/v1/warehouses')
+      const result = await response.json()
+      console.log(result)
+
+      return this.warehouses = result
+    }
+  }
 }
 </script>
 
